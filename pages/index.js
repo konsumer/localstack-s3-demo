d77746021cca05bb61173b0c09bf2e9da6f0221c
David Konsumer <konsumer@jetboystudio.com>
2020-09-27 00:24:36 -0700

    even simpler form

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 /* global fetch, FileReader */
-import React from 'react'
+import React, { useState } from 'react'
 
 const getSignedUrl = file => fetch('/api', {
   method: 'POST',
@@ -7,26 +7,39 @@ const getSignedUrl = file => fetch('/api', {
   headers: { 'content-type': 'application/json' }
 }).then(r => r.text())
 
-const readFile = file => new Promise((resolve, reject) => {
+const readAsDataURL = file => new Promise((resolve, reject) => {
   const reader = new FileReader()
   reader.onload = e => resolve(e.target.result)
   reader.readAsDataURL(file)
 })
 
-const handleSubmit = async e => {
-  e.preventDefault()
-  for (const file of e.target.file.files) {
-    const signedUrl = await getSignedUrl(file)
-    const r = await fetch(signedUrl, { method: 'PUT', body: file })
-    console.log(r)
-  }
-}
-
 const PageIndex = () => {
+  const [images, setImages] = useState([])
+  const [errors, setErrors] = useState([])
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const newImages = []
+    const newErrors = []
+    for (const file of e.target.file.files) {
+      const signedUrl = await getSignedUrl(file)
+      const r = await fetch(signedUrl, { method: 'PUT', body: file })
+      if (r.status === 200) {
+        newImages.push(await readAsDataURL(file))
+      } else {
+        newErrors.push(`Error (${r.status}) uploading ${file.name}`)
+      }
+    }
+    setImages(newImages)
+    setErrors(newErrors)
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <input type='file' name='file' />
       <button type='submit'>upload</button>
+      {errors.map((error, e) => (<div key={e} style={{ color: 'red' }}>{error}</div>))}
+      {images.map((image, i) => <img key={i} src={image} />)}
     </form>
   )
 }
